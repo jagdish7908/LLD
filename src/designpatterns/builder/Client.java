115f0b4e8c6923cc8d37a54d4ac66322d99ee5f2
Jagdish Chaudhary <jchaudhary7908@gmail.com>
2023-08-13 11:52:09 +0530

    builder v3: method chaining to reduce duplicate code

--- src/designpatterns/builder/Client.java
+++ src/designpatterns/builder/Client.java
@@ -2,13 +2,11 @@ package designpatterns.builder;
 
 public class Client {
     public static void main(String[] args) {
-        StudentBuilder builder = Student.getBuilder();
-        builder.setName("John");
-        builder.setCollegeName("MIT");
-        builder.setScore(95);
-
-        // create/build object using builder
-        Student s = Student.build(builder);
+        Student s = Student.getBuilder()
+                            .setName("John")
+                            .setCollegeName("MIT")
+                            .setScore(95)
+                            .build();
 
         System.out.println("Name: " + s.getName());
         System.out.println("College Name: " + s.getCollegeName());
--- src/designpatterns/builder/Student.java
+++ src/designpatterns/builder/Student.java
@@ -29,7 +29,38 @@ public class Student {
         return new StudentBuilder();
     }
 
-    public static Student build(StudentBuilder builder) {
-        return new Student(builder);
+    public static class StudentBuilder {
+        private String name;
+        private String collegeName;
+        private int score;
+
+        public String getName() {
+            return name;
+        }
+
+        public  String getCollegeName() {
+            return collegeName;
+        }
+
+        public int getScore() { return score; }
+
+        public Student build() {
+            return new Student(this);
+        }
+
+        public StudentBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public StudentBuilder setCollegeName(String collegeName) {
+            this.collegeName = collegeName;
+            return this;
+        }
+
+        public StudentBuilder setScore(int score) {
+            this.score = score;
+            return this;
+        }
     }
 }
